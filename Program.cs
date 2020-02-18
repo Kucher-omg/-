3d0ac694500a191c695f2242796af9af1f29d62e
Vlad <vladkycherenko98@gmail.com>
2020-02-18 17:01:31 +0200

    Step 1-8, maybe last)

--- Program.cs
+++ Program.cs
@@ -24,12 +24,11 @@ namespace Курсова
         }
         static void Print ( double [,] a)
         {
-           // Console.WriteLine("your matrix is");
             for (int i = 0; i < a.GetLength(0); i++)
             {
                 for (int j = 0; j < a.GetLength(1); j++)
                 {
-                    Console.Write($"{a[i,j]} ");
+                    Console.Write($"{Math.Round(a[i, j],4)} ");
                 }
                 Console.WriteLine();
             }
@@ -82,6 +81,171 @@ namespace Курсова
             return result;
         }
 
+        public static double[,] Inverted(double[,] matrix)
+        {
+            int m = matrix.GetLength(0);
+            int n = matrix.GetLength(1);
+
+            double[,] res = new double[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    res[i, j] = matrix[i, j];
+                }
+            }
+
+            if (n != m)
+            {
+                throw new Exception("Inverted matrix can`t be");
+            }
+
+            int[] row = new int[n];
+            int[] col = new int[n];
+
+            double[] temp = new double[n];
+            int hold;
+            int I_pivot;
+            int J_pivot;
+            double pivot;
+            double abs_pivot;
+
+            for (int k = 0; k < n; k++)
+            {
+                row[k] = k;
+                col[k] = k;
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+
+                pivot = res[row[k], col[k]];
+                I_pivot = k;
+                J_pivot = k;
+                for (int i = k; i < n; i++)
+                {
+                    for (int j = k; j < n; j++)
+                    {
+                        abs_pivot = Math.Abs(pivot);
+                        if (Math.Abs(res[row[i], col[j]]) > abs_pivot)
+                        {
+                            I_pivot = i;
+                            J_pivot = j;
+                            pivot = res[row[i], col[j]];
+                        }
+                    }
+                }
+
+                if (Math.Abs(pivot) < 1.0E-10)
+                {
+                    
+                    throw new Exception("!");
+                }
+
+                hold = row[k];
+                row[k] = row[I_pivot];
+                row[I_pivot] = hold;
+                hold = col[k];
+                col[k] = col[J_pivot];
+                col[J_pivot] = hold;
+                
+                res[row[k], col[k]] = 1.0 / pivot;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (j != k)
+                    {
+                        res[row[k], col[j]] = res[row[k], col[j]] * res[row[k], col[k]];
+                    }
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (k != i)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (k != j)
+                            {
+                                res[row[i], col[j]] = res[row[i], col[j]] - res[row[i], col[k]] * res[row[k], col[j]];
+                            }
+                        }
+                        res[row[i], col[k]] = -res[row[i], col[k]] * res[row[k], col[k]];
+                    }
+                }
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    temp[col[i]] = res[row[i], j];
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    res[i, j] = temp[i];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    temp[row[j]] = res[i, col[j]];
+                }
+
+                for (int j = 0; j < n; j++)
+                {
+                    res[i, j] = temp[j];
+                }
+            }
+
+            return res;
+        }
+
+        public static double Rank(double[,] matrix)
+        {
+            int rang = 0;
+            int q = 1;
+
+            while (q <= MinValue(matrix.GetLength(0), matrix.GetLength(1)))
+            {
+                double[,] matbv = new double[q,q];
+                for (int i = 0; i < (matrix.GetLength(0) - (q - 1)); i++)
+                {
+                    for (int j = 0; j < (matrix.GetLength(1) - (q - 1)); j++)
+                    {
+                        for (int k = 0; k < q; k++)
+                        {
+                            for (int c = 0; c < q; c++)
+                            {
+                                matbv[k, c] = matrix[i + k, j + c];
+                            }
+                        }
+
+                        if (DetRec(matbv) != 0)
+                        {
+
+                            rang = q;
+                        }
+                    }
+                }
+                q++;
+            }
+
+            return rang;
+        }
+
+        private static int MinValue(int a, int b)
+        {
+            if (a >= b)
+                return b;
+            else
+                return a;
+        }
+
         static void Main(string[] args)
         {
             ConsoleKey key;
@@ -92,9 +256,10 @@ namespace Курсова
                 Console.Write("List:\n 1 - Add and subtract matrix\n" +
                     " 2 - Transposition of matrix\n 3 - Multiplication of matrix\n" +
                     " 4 - Rise of the matrix to the degree\n 5 - Determinant of matrix\n" +
-                    " 6 - Inverted matrix\n 7 - Multiplay matrix on character\nYour answer is ");
+                    " 6 - Inverted matrix\n 7 - Multiplay matrix by number\n" +
+                    " 8 - Rank of matrix\nYour answer is ");
                 int Operations;
-                while ((!int.TryParse(Console.ReadLine(), out Operations)) || Operations < 1 || Operations > 7)
+                while ((!int.TryParse(Console.ReadLine(), out Operations)) || Operations < 1 || Operations > 8)
                 {
                     Console.Write("Inccorrect input, choose operations -> ");
 
@@ -127,7 +292,7 @@ namespace Курсова
                                     Console.Write($"mas[{i + 1},{j + 1}] = ");
                                     while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
                                     {
-                                        Console.Write($"Unncorect input, mas[{i + 1},{j} + 1] = ");
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
                                     }
                                 }
                             }
@@ -214,7 +379,7 @@ namespace Курсова
                                     Console.Write($"mas[{i + 1},{j + 1}] = ");
                                     while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
                                     {
-                                        Console.Write($"Unncorect input, mas[{i + 1},{j} + 1] = ");
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
                                     }
                                 }
                             }
@@ -276,7 +441,7 @@ namespace Курсова
                                     Console.Write($"mas[{i + 1},{j + 1}] = ");
                                     while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
                                     {
-                                        Console.Write($"Unncorect input, mas[{i + 1},{j} + 1] = ");
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
                                     }
                                 }
                             }
@@ -319,7 +484,7 @@ namespace Курсова
                                     Console.Write($"mas[{i + 1},{j + 1}] = ");
                                     while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
                                     {
-                                        Console.Write($"Unncorect input, mas[{i + 1},{j} + 1] = ");
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
                                     }
                                 }
                             }
@@ -365,7 +530,7 @@ namespace Курсова
                                     Console.Write($"mas[{i + 1},{j + 1}] = ");
                                     while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
                                     {
-                                        Console.Write($"Unncorect input, mas[{i + 1},{j} + 1] = ");
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
                                     }
                                 }
                             }
@@ -399,20 +564,24 @@ namespace Курсова
                                     Console.Write($"mas[{i + 1},{j + 1}] = ");
                                     while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
                                     {
-                                        Console.Write($"Unncorect input, mas[{i + 1},{j} + 1] = ");
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
                                     }
                                 }
                             }
                             Console.WriteLine("Your matrix is");
                             Print(array1);
 
+                            double[,] result = Inverted(array1);
+                            Console.WriteLine("Result is");
+                            Print(result);
+
                         } break;
 
                     case 7://Multiplay2
                         {
-                            int n;
-                            Console.Write("Enter the character -> ");
-                            while ((!int.TryParse(Console.ReadLine(), out n)) || n < 1 || n > 10)
+                            double n;
+                            Console.Write("Enter the number -> ");
+                            while ((!double.TryParse(Console.ReadLine(), out n)) )
                             {
                                 Console.Write("Enter correct character -> ");
 
@@ -440,7 +609,7 @@ namespace Курсова
                                     Console.Write($"mas[{i + 1},{j + 1}] = ");
                                     while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
                                     {
-                                        Console.Write($"Unncorect input, mas[{i + 1},{j} + 1] = ");
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
                                     }
                                 }
                             }
@@ -453,6 +622,49 @@ namespace Курсова
                             }
                             Console.WriteLine("Your matrix is");
                             Print(array1);
+                        } break;
+                    case 8://Rang
+                        {
+                            int rawsAmount;
+                            Console.Write("Enter the amount of raws -> ");
+                            while ((!int.TryParse(Console.ReadLine(), out rawsAmount)) || rawsAmount < 1 || rawsAmount > 10)
+                            {
+                                Console.Write("Enter correct amount of raws -> ");
+
+                            }
+                            int colomsAmount;
+                            Console.Write("Enter the amount of coloms -> ");
+                            while ((!int.TryParse(Console.ReadLine(), out colomsAmount)) || colomsAmount < 1 || colomsAmount > 10)
+                            {
+                                Console.Write("Enter correct amount of coloms -> ");
+                            }
+
+                            double[,] array1 = new double[rawsAmount, colomsAmount];
+
+                            Console.WriteLine("Enter matrix");
+                            for (int i = 0; i < rawsAmount; i++)
+                            {
+                                for (int j = 0; j < colomsAmount; j++)
+                                {
+                                    Console.Write($"mas[{i + 1},{j + 1}] = ");
+                                    while ((!double.TryParse(Console.ReadLine(), out array1[i, j])))
+                                    {
+                                        Console.Write($"Unncorect input, mas[{i + 1},{j + 1}] = ");
+                                    }
+                                }
+                            }
+
+                            Console.WriteLine("Your matrix is ");
+                            Print(array1);
+
+                            double rang = Rank(array1);
+
+                            if (rang == 0)
+                            {
+                                Console.WriteLine($"The rank of this matrix is 1");
+                            }
+                            else Console.WriteLine($"The rank of this matrix is {rang}");
+
                         } break;
                 }
 
